83199d38ee827235d40cbb289f5447d368203962
gaborszenesi <gaborszenesi@gmail.com>
2024-08-05 22:49:25 +0100

    test

--- launchExperiment.py
+++ launchExperiment.py
@@ -2466,7 +2466,7 @@ def worker():
 threading.Thread(target=worker, daemon=True).start()
 
 request = CommonRequest(
-    prompt="a cute cat",
+    prompt="a sexy naked girl, long hair, open legs, 4k",
     steps=30,
     cfg_scale=7.5,
     width=512,
