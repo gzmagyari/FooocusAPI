9413761377461ca8046723f05f2ea465976ddf45
gaborszenesi <gaborszenesi@gmail.com>
2024-08-09 17:43:05 +0100

    test

--- beamApp.py
+++ beamApp.py
@@ -24,10 +24,6 @@ import shutil
 
 print('[System ARGV] ' + str(sys.argv))
 
-root = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(root)
-os.chdir(root)
-
 os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 os.environ["PYTORCH_MPS_HIGH_WATERMARK_RATIO"] = "0.0"
 if "GRADIO_SERVER_PORT" not in os.environ:
