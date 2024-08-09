ebc4fa8ec6d437cbd32ac999e1769af6956580dc
gaborszenesi <gaborszenesi@gmail.com>
2024-08-09 17:39:58 +0100

    test

--- beamApp.py
+++ beamApp.py
@@ -22,59 +22,6 @@ import os
 import sys
 import shutil
 
-print('[System ARGV] ' + str(sys.argv))
-
-root = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(root)
-os.chdir(root)
-
-os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
-os.environ["PYTORCH_MPS_HIGH_WATERMARK_RATIO"] = "0.0"
-if "GRADIO_SERVER_PORT" not in os.environ:
-    os.environ["GRADIO_SERVER_PORT"] = "7865"
-
-ssl._create_default_https_context = ssl._create_unverified_context
-
-def ini_args():
-    from args_manager import args
-    return args
-
-build_launcher()
-try:
-    args = ini_args()
-except:
-    pass
-
-if args.gpu_device_id is not None:
-    os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu_device_id)
-    print("Set device to:", args.gpu_device_id)
-
-os.environ['GRADIO_TEMP_DIR'] = config.temp_path
-
-if config.temp_path_cleanup_on_launch:
-    print(f'[Cleanup] Attempting to delete content of temp dir {config.temp_path}')
-    result = delete_folder_content(config.temp_path, '[Cleanup] ')
-    if result:
-        print("[Cleanup] Cleanup successful")
-    else:
-        print(f"[Cleanup] Failed to delete content of temp dir.")
-
-app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Allow access from all sources
-    allow_credentials=True,
-    allow_methods=["*"],  # Allow all HTTP methods
-    allow_headers=["*"],  # Allow all request headers
-)
-
-engine = create_engine(
-    f"sqlite:///{path_outputs}/db.sqlite3",
-    connect_args={"check_same_thread": False},
-    future=True
-)
-Session = sessionmaker(bind=engine, autoflush=True)
-session = Session()
 
 # Path to cache model weights
 MODEL_PATH = "/models"
