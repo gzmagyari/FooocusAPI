9c480795043ab6f6734f063c9ac0d504fc12a818
gaborszenesi <gaborszenesi@gmail.com>
2024-08-09 11:34:20 +0100

    fixes

--- beamApp.py
+++ beamApp.py
@@ -1,80 +1,53 @@
 import os
-import ssl
-import json
 import asyncio
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import json
 from beam import endpoint, Image, Volume, env, Output
 from urllib.parse import urlparse
 from typing import Optional, Dict, List
-
-from build_launcher import build_launcher
-from modules.launch_util import delete_folder_content
-from apis.models.requests import CommonRequest
-from modules.config import path_outputs
-from apis.utils.img_utils import base64_to_image
-from modules import config
 from classes.FooocusModel import FooocusModel
+from apis.models.requests import CommonRequest
+from apis.utils.img_utils import base64_to_image
 from makeModelDictionary import makeModelDictionary
-import os
-import sys
 
+def initializeApp():
+    import os
+    import ssl
+    from build_launcher import build_launcher
+    from modules import config
+    import os
+    import sys
 
-print('[System ARGV] ' + str(sys.argv))
+    print('[System ARGV] ' + str(sys.argv))
 
-root = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(root)
-os.chdir(root)
+    root = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(root)
+    os.chdir(root)
 
-os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
-os.environ["PYTORCH_MPS_HIGH_WATERMARK_RATIO"] = "0.0"
-if "GRADIO_SERVER_PORT" not in os.environ:
-    os.environ["GRADIO_SERVER_PORT"] = "7865"
+    os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+    os.environ["PYTORCH_MPS_HIGH_WATERMARK_RATIO"] = "0.0"
+    if "GRADIO_SERVER_PORT" not in os.environ:
+        os.environ["GRADIO_SERVER_PORT"] = "7865"
 
-ssl._create_default_https_context = ssl._create_unverified_context
+    ssl._create_default_https_context = ssl._create_unverified_context
 
-def ini_args():
-    from args_manager import args
-    return args
+    def ini_args():
+        from args_manager import args
+        return args
 
-build_launcher()
-try:
-    args = ini_args()
-except:
-    pass
+    build_launcher()
+    try:
+        args = ini_args()
+    except:
+        pass
 
-if args.gpu_device_id is not None:
-    os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu_device_id)
-    print("Set device to:", args.gpu_device_id)
+    if args.gpu_device_id is not None:
+        os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu_device_id)
+        print("Set device to:", args.gpu_device_id)
 
-os.environ['GRADIO_TEMP_DIR'] = config.temp_path
+    os.environ['GRADIO_TEMP_DIR'] = config.temp_path
 
-if config.temp_path_cleanup_on_launch:
-    print(f'[Cleanup] Attempting to delete content of temp dir {config.temp_path}')
-    result = delete_folder_content(config.temp_path, '[Cleanup] ')
-    if result:
-        print("[Cleanup] Cleanup successful")
-    else:
-        print(f"[Cleanup] Failed to delete content of temp dir.")
-
-app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Allow access from all sources
-    allow_credentials=True,
-    allow_methods=["*"],  # Allow all HTTP methods
-    allow_headers=["*"],  # Allow all request headers
-)
-
-engine = create_engine(
-    f"sqlite:///{path_outputs}/db.sqlite3",
-    connect_args={"check_same_thread": False},
-    future=True
-)
-Session = sessionmaker(bind=engine, autoflush=True)
-session = Session()
+    return load_model()
+    
 
 # Path to cache model weights
 MODEL_PATH = "/models"
@@ -120,7 +93,7 @@ volume = Volume(name="fooocus_model_cache", mount_path=MODEL_PATH)
 @endpoint(
     name="fooocus-ai-service",
     volumes=[volume],
-    on_start=load_model,
+    on_start=initializeApp,
     image=Image(
         python_version="python3.10",
         python_packages=[
@@ -191,7 +164,7 @@ if __name__ == "__main__":
     if env.is_remote():
         generate_image.serve()
     else:
-        model = load_model()
+        model = initializeApp()
         request = CommonRequest(
             prompt="a cute cat, crisp clear, 4k, vivid colors, high resolution",
             negative_prompt="blurry, low resolution, pixelated",
