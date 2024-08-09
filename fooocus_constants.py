77e1ac4ac3c46e5dfe04fa42503c76eec87ac65d
gaborszenesi <gaborszenesi@gmail.com>
2024-08-10 00:19:43 +0100

    beam

--- fooocus_constants.py
+++ fooocus_constants.py
@@ -5,7 +5,7 @@ VOLUME_OUTPUT_DIR = os.path.join(VOLUME_MODEL_PATH, "outputs")
 VOLUME_INPUT_DIR = os.path.join(VOLUME_MODEL_PATH, "inputs")
 #LOCAL_MODEL_PATH = os.path.join(os.path.dirname(__file__), 'models')
 LOCAL_MODEL_PATH = "/workspace/models"
-LOCAL_MODEL_PATH = "/root/FooocusAPI/models"
+#LOCAL_MODEL_PATH = "/root/FooocusAPI/models"
 USE_VOLUME_FOR_CHECKPOINTS = False
 
 os.makedirs(VOLUME_MODEL_PATH, exist_ok=True)
