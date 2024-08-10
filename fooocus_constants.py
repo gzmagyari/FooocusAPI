46fb042beca3b842c611a0a70d50f2ebd561a79d
gaborszenesi <gaborszenesi@gmail.com>
2024-08-10 13:35:29 +0100

    modal

--- fooocus_constants.py
+++ fooocus_constants.py
@@ -1,10 +1,12 @@
 import os
 
-VOLUME_MODEL_PATH = "/volumes/fooocus_model_cache"
+#VOLUME_MODEL_PATH = "/volumes/fooocus_model_cache"
+VOLUME_MODEL_PATH = "/fooocus_model_cache"
 VOLUME_OUTPUT_DIR = os.path.join(VOLUME_MODEL_PATH, "outputs")
 VOLUME_INPUT_DIR = os.path.join(VOLUME_MODEL_PATH, "inputs")
 #LOCAL_MODEL_PATH = os.path.join(os.path.dirname(__file__), 'models')
-LOCAL_MODEL_PATH = "/workspace/models"
+#LOCAL_MODEL_PATH = "/workspace/models"
+LOCAL_MODEL_PATH = "/fooocus_model_cache"
 #LOCAL_MODEL_PATH = "/root/FooocusAPI/models"
 USE_VOLUME_FOR_CHECKPOINTS = False
 
